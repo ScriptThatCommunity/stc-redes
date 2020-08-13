102737a67895225381f3ce961f021d3743caf346
JSCode-dev <69640842+JSCode-dev@users.noreply.github.com>
2020-08-13 17:55:41 +0200

    Añadidos los scripts

---
+++ __resource.lua
@@ -0,0 +1,5 @@
+-- Script Programado por ScriptThat Community | https://discord.gg/JtcTffs
+
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+client_script "client.lua"
---
+++ config.lua
@@ -0,0 +1,46 @@
+--  _____           _       _ _______ _           _   
+--  / ____|         (_)     | |__   __| |         | |  
+--  | (___   ___ _ __ _ _ __ | |_ | |  | |__   __ _| |_ 
+--  \___ \ / __| '__| | '_ \| __|| |  | '_ \ / _` | __|
+--  ____) | (__| |  | | |_) | |_ | |  | | | | (_| | |_ 
+--  |_____/ \___|_|  |_| .__/ \__||_|  |_| |_|\__,_|\__|
+--                    | |                              
+--                    |_|                              
+--   _____                                      _ _         
+--   / ____|                                    (_) |        
+--   | |     ___  _ __ ___  _ __ ___  _   _ _ __  _| |_ _   _ 
+--   | |    / _ \| '_ ` _ \| '_ ` _ \| | | | '_ \| | __| | | |
+--   | |___| (_) | | | | | | | | | | | |_| | | | | | |_| |_| |
+--   \_____\___/|_| |_| |_|_| |_| |_|\__,_|_| |_|_|\__|\__, |
+--                                                      __/ |
+--                                                      |___/
+--  ©2020 ScriptThat Community
+--  No se permite la venda de este script en ningún lugar ni publicar este script en otras páginas.
+--  Discord: https://discord.gg/JtcTffs
+
+svname = {     
+  svname="TuservidorRP"     -- Aqui va el nombre de tu servidor
+}
+
+twitter = {     
+  twitter="twitter.com/"      -- Aqui va tu link de twitter
+  }
+
+instagram = {     
+  instagram="instagram.com/"     -- Aqui va tu link de instagram
+  }
+
+discord = {     
+  discord="discord.gg/"     -- Aqui va tu link de discord
+  }
+
+
+-- Si quieres añadir otro, puedes poner aqui el link y el nombre 
+other1name = {
+  other1name=""     -- Nombre del otro que quieras añadir | EL NOMBRE DEBE TENER ":" AL FINAL, EJEMPLO "Web:"
+}
+other1link = {
+    other1link=""     -- Link del otro que quieras añadir
+  }
+
+-- Si despues quieres añdir más, abrenos un ticket en Discord y te ayudamos
